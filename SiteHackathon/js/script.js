c57ea0d46cf900d4685e3a33b6a06eae7377608c
facerennes <facerennes>
2017-11-15 11:30:49 +0100

    Prob Boostrap responsive col-sm

---
+++ SiteHackathon/js/script.js
@@ -0,0 +1,32 @@
+// SLIDE
+var deleteLog = false;
+
+$(document).ready(function () {
+    $('#pagepiling').pagepiling({
+        menu: '#menu',
+        anchors: ['page1', 'page2', 'page3', 'page4'],
+        sectionsColor: ['black', 'black', 'black', 'black'],
+        loopTop: true,
+        loopBottom: true
+    });
+});
+
+
+// NAVBAR
+$("#mostrar-nav").on("click", function () {
+    $("nav").toggleClass("mostrar");
+});
+
+
+// EFFET TEXTE TITLE
+var myText = 'organisé par la Code Académie',
+    i = 0;
+window.onload = function () {
+    var typeWriter = setInterval(function () {
+        document.getElementById('text').textContent += myText[i];
+        i++;
+        if (i > myText.length - 1) {
+            clearInterval(typeWriter);
+        }
+    }, 150);
+};
