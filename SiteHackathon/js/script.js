596978a2d33d729b36af0eb9792f55ad66156571
facerennes <facerennes>
2017-11-27 14:22:11 +0100

    Revision de l'aspect visuel du site, ajustement

--- SiteHackathon/js/script.js
+++ SiteHackathon/js/script.js
@@ -19,7 +19,7 @@ $("#mostrar-nav").on("click", function () {
 
 
 // EFFET TEXTE TITLE
-var myText = "organisé par la Code Académie et l'Epita",
+var myText = "Ici on inscrit les dates du Hackathon",
     i = 0;
 window.onload = function () {
     var typeWriter = setInterval(function () {
