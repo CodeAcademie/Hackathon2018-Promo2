ebb30a85ba0fabc6f71d53f1ca9b01020ceeb4f1
facerennes <facerennes>
2017-11-29 16:05:01 +0100

    Responsive Ok

--- SiteHackathon/js/responsive.js
+++ SiteHackathon/js/responsive.js
@@ -1,6 +1,5 @@
-
-     if ($(window).width() >= 699) {
-         $('head').append('<script type="text/javascript" src="js/jquery.pagepiling.min.js" id="haaaa"></script>');
-     } else if ($(window).width() < 699) {
-         $('#haaaa').remove('<script type="text/javascript" src="js/jquery.pagepiling.min.js"></script>');
-     }
+// location.reload();
+if ($(window).width() >= 699) {
+     $('head').append('<script type="text/javascript" src="js/jquery.pagepiling.min.js" id="haaaa"></script>');
+} ;
+     
