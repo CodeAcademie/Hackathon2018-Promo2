34d453d957fdb57fdad4132d84c631a949d029da
facerennes <codeacademie35@gmail.com>
2017-12-21 15:04:22 +0100

    ajustement CSS

--- Site_Hack/index.php
+++ Site_Hack/index.php
@@ -118,20 +118,8 @@
                         <img src="assets/imgs/lorem-ipsum1.jpg" alt="alt-?" class="img-calendar">
                         <h3><img src="assets/imgs/004-circle-button.svg" alt="Circle Button"> Voter pour vos thèmes !</h3>
                         <div class="orange"></div>
-                        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc8pKQatOejthE8gc_SBsae8eXGMBRSXncwmoIWpGkfvriSJQ/viewform?embedded=true" width="100%" height="500" frameborder="0" marginheight="0" marginwidth="0">Chargement en cours...</iframe>
-                      <!--  <ul class="timeline">
-                            <li>
-                                <img src="assets/imgs/invaders/invaders9.svg" alt="Space Invaders">
-                                <h4>09:15 &#45; Petit déjeuner</h4>
-                            </li>
-                            <li>
-                                <img src="assets/imgs/invaders/invaders4.svg" alt="Space Invaders">
-                                <h4>?:? &#45; Lorem Ipsum</h4>
-                            </li>
-                            <li>
-                                <img src="assets/imgs/invaders/invaders4.svg" alt="Space Invaders">
-                                <h4>?:? &#45; Lorem Ipsum</h4>
-                            </li>
+                        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc8pKQatOejthE8gc_SBsae8eXGMBRSXncwmoIWpGkfvriSJQ/viewform?embedded=true" width="100%" height="325" frameborder="0" marginheight="0" marginwidth="0">Chargement en cours...</iframe>
+                        <ul class="timeline">
                             <li>
                                 <img src="assets/imgs/invaders/invaders4.svg" alt="Space Invaders">
                                 <h4>?:? &#45; Lorem Ipsum</h4>
@@ -140,15 +128,7 @@
                                 <img src="assets/imgs/invaders/invaders5.svg" alt="Space Invaders">
                                 <h4>?:? &#45; Lorem Ipsum</h4>
                             </li>
-                            <li>
-                                <img src="assets/imgs/invaders/invaders10.svg" alt="Space Invaders">
-                                <h4>13:30 &#45; Présentation du projet au jury</h4>
-                            </li>
-                            <li>
-                                <img src="assets/imgs/invaders/invaders11.svg" alt="Space Invaders">
-                                <h4>15:00 &#45; Fermeture des locaux</h4>
-                            </li>
-                        </ul>-->
+                        </ul>
                     </article>
                     <article class="col-md-4">
                         <h3><img src="assets/imgs/001-square-button.svg" alt="Square Button"> Vendredi 16 Fevrier</h3>
